a0e89606002683de5b3a531ee8062b0e892029cb
Diego <policarpo16@yahoo.com.br>
2022-08-13 02:11:12 -0300

    Inicio do projeto

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# API BLUESTORM
-
-# APP_DLX-Backend
-# APP_DLX-Backend
 # APP_DLX-Backend
---
+++ src/models/models.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class Base(BaseModel):
+    orm_mode = True
+
+class Usuario(Base):
+    id: Optional[str] = None
+    nome: str
+    telefone: str
+    meus_produtos: List[Produtos]
+    minhas_vendas: List[Pedido]
+    meus_pedidos: List[Pedido]
+
+class Produto(Base):
+    id: Optional[str] = None
+    usuario: Usuario
+    nome: str
+    detalhes: str
+    preco: float
+    disponivel: bool = False
+
+class Pedido(Base):
+    id: Optional[str] = None
+    usuario: Usuario
+    produto: Produto
+    quantidade: int
+    entrega: bool = True
+    endereco: str
+    observacoes: Optional[str] = 'Sem observações'
+
+
